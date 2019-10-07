241c722b65b0dfb4fd077a1bf6985db2038e3a7e
Marwan Helali <marwan01@Marwans-MacBook-Pro.local>
2019-10-07 17:47:11 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
+
 git add .
 git commit -m "deploy script"
 git push
 
 ng build --prod
+
+pip install BeautifulSoup4
+pip install requests
+pip install tqdm
+pip install re
+./scraping.py
 
 #/bin/bash
 #upload files
