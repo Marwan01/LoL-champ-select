75f64ec2d7f5191ca1c521c4d0d9482271afda5e
Marwan01 <marwanest@live.fr>
2019-10-07 13:58:46 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ ng build --prod
 #/bin/bash
 #upload files
 
-aws s3 cp ./dist s3://lol-champ-select --recursive --acl public-read
+aws s3 cp ./dist/ChampSelect s3://lol-champ-select --recursive --acl public-read
