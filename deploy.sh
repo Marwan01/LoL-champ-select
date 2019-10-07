399b3f3af19beb0685e63053a7e777fb78c32f07
Marwan01 <marwanest@live.fr>
2019-10-07 13:36:41 -0500

    updated deploy script :cd:

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+git add .
+git commit -m "deploy script"
+git push
+
+ng build --prod
+
 #/bin/bash
 #upload files
-aws s3 cp ./dist s3://cloud.lol-champ-select.com --recursive --acl public-read
+
+aws s3 cp ./dist s3://lol-champ-select --recursive --acl public-read
